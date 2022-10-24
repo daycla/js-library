9d23bdf659ac5c240e7b99edfe9c3c605b31673d
daycla <112831236+daycla@users.noreply.github.com>
2022-10-24 02:26:14 -0500

    form skeleton built

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//each entry
+//div container (span.title span.author span.pages etc)
+
+//when new book button clikced
+//slide main down, revealing form which is "behind " it in space under header
+
+const nbButton = document.querySelector('#newBookButton');
+
+nbButton.addEventListener('click', enterNewBook());
+
+function enterNewBook() {
+    
+};
