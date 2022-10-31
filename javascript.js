9f16e5ad094706976efa7fb673faea7a33442683
daycla <112831236+daycla@users.noreply.github.com>
2022-10-30 21:21:42 -0500

    creates proper DOM elements from library

--- javascript.js
+++ javascript.js
@@ -22,4 +22,75 @@ const formSubmit = document.querySelector('#formSubmit');
 
 let myLibrary = [];
 
+exampleOne = new Book("Left Hand", "Ursula", 300, false);
+exampleTwo = new Book("Limoncello", "Lacroix", 0, true);
+exampleThree = new Book("Marriage of Heaven and Hell", "William Blake", 150, true);
+
+myLibrary.push(exampleOne);
+myLibrary.push(exampleTwo);
+myLibrary.push(exampleThree);
+
+newBookButton.addEventListener('click', () => newBookForm.style.display = 'flex');
+
+function Book(title, author, pages, readYet) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.readYet = readYet
+}   
+
+function fillBookshelf() {
+    myLibrary.map((book, index) => {
+        createBookItem(book, index);
+    })
+
+    addBookTools();
+}
+
+function createBookItem(book, index) {
+
+    const bookItem = document.createElement('div');
+    bookItem.setAttribute('id', index)
+    bookItem.setAttribute('class', 'book');
+
+    const bookTitle = document.createElement('h1');
+    bookTitle.textContent = book.title;
+    bookTitle.setAttribute('class', 'bookTitle');
+
+    const bookAuthor = document.createElement('h1');
+    bookAuthor.textContent = book.author;
+    bookAuthor.setAttribute('class', 'bookAuthor');
+
+    const bookPages = document.createElement('h1');
+    bookPages.textContent = `${book.pages} pages`;
+    bookPages.setAttribute('class', 'bookPages');
+
+    bookItem.appendChild(bookTitle);
+    bookItem.appendChild(bookAuthor);
+    bookItem.appendChild(bookPages);
+
+    booksList.appendChild(bookItem);
+
+}
+
+function addBookTools() {
+
+    const bookItems = Array.from(document.querySelectorAll('.book'));
+    console.log(bookItems)
+
+    bookItems.map((book) => {
+
+        const bookToolsWrapper = document.createElement('div');
+        bookToolsWrapper.setAttribute('class', 'bookToolsWrapper');
+        book.appendChild(bookToolsWrapper);
+
+        const bookEdit = document.createElement('button');
+        bookEdit.setAttribute('class', 'bookTools');
+        bookEdit.setAttribute('id', 'bookEdit')
+        bookToolsWrapper.appendChild(bookEdit);
+
+    })
+}
+
+fillBookshelf();
 
